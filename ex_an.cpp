36ceb9b8c738ebb68587fc90c16556c8fe36d681
Allen Ling <87615269+Lcyallen@users.noreply.github.com>
2021-07-19 09:41:26 +0800

    列出等差數列
    輸入首項、末項和項數，把等差數列列出來

---
+++ ex_an.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+	
+	int a, an, d;
+	
+	cin>>a>>an>>d;
+	
+	while (a<=an)
+	{
+		cout<<a<<" ";
+				
+		a=a+d;
+	}
+		cout<<endl;
+		
+		return 0;
+}
