d21598aee42abaf9bf02cf69c26b53c4b34f3ae6
Allen Ling <87615269+Lcyallen@users.noreply.github.com>
2021-07-21 00:39:36 +0800

    用*排三角形
    1.https://www.csie.ntu.edu.tw/~b98902112/cpp_and_algo/cpp02/practice_rectangular_triangle_maker_sol.html
    2.這個題目是當年大一修計概時老師出的題目，我當年想不出來。現在仍然想不出來，是看解答才知道的...

---
+++ right triangle.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main (){
+	
+	int i, j, n;
+	
+	cin>>n;
+	
+	for (j=1; j<=n; j=j+1)
+	{
+		for (i=1; i<=j; i=i+1)	
+			cout<<"*";
+			
+		cout<<endl;
+	}
+		
+	return 0;
+}
+
+
