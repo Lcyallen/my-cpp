59533244e56508a9cf667d310cb4363cf264591b
Allen Ling <87615269+Lcyallen@users.noreply.github.com>
2021-07-20 17:29:35 +0800

    列出0_1數列
    1.老師課堂上給的作業
    2.看似簡單，但變數i那裡還是錯了2, 3次

---
+++ 01series.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main (){
+	
+	int i;
+	
+	i=1;
+	while (i<=35)
+	{
+		if (i%2!=0)
+			cout<<1<<" ";
+		
+		else
+			cout<<0<<" ";
+		
+		i=i+1;
+	}
+	
+	cout<<endl;	
+
+		return 0;
+}
+
+
