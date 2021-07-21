10c35d807cda23c835418fe4201ebfca941b0ea7
Allen Ling <87615269+Lcyallen@users.noreply.github.com>
2021-07-21 15:55:30 +0800

    找出數列中的最大值
    1.同學寫的答案，又簡短又漂亮！
    2.我一直卡在如何讓輸入值一個一個進行處理，以為一定要透過i來控制，其實根本不用...
    3.對於程式運算的邏輯還需要再多練習呀~~

---
+++ Max of the series_2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main (){
+	
+	int a, M;
+	
+	while (cin>>a)
+	{
+		if (M<a)
+			M=a;
+		}
+	
+	cout<<M<<endl;
+	
+	return 0;
+}
+
+//這是同學的解答，多漂亮！我對於i的理解要再多加強~~ 
+
