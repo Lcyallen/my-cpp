346e09a3437b002c46c0a141058813b19b1caa83
Allen Ling <87615269+Lcyallen@users.noreply.github.com>
2021-07-22 23:15:17 +0800

    數了幾隻羊
    1.https://docs.google.com/presentation/d/1wRpEaIhYa3yH4qZyOq4Qw77loUm0ZQxSSyRjILTQUc8/present?slide=id.p9
    2.蠻好玩的

---
+++ how many sheeps.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main (){
+	
+	int n, m, sum, i;
+	
+	cin>>n>>m;
+	
+	sum=n; i=1;
+	
+	while (sum<=m)
+	{
+		i++;
+		n++; 
+		sum=sum+n;
+	}
+	
+	cout<<i<<endl;
+	
+		return 0;
+}
+
+//*Τ⊿Τ快猭ぃノ皚т计程程 
+
