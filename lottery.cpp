632f9183e9a368e8124b3ed248bdd25e1ac9b2e6
Allen Ling <87615269+Lcyallen@users.noreply.github.com>
2021-07-31 15:58:35 +0800

    是否中獎
    1.http://www.tcgs.tc.edu.tw:1218/ShowProblem?problemid=a019的解答
    2.一開始沒注意到「沒中獎」的情況

---
+++ lottery.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+ 	int a, b;
+ 
+ 	cin>>a>>b;
+ 
+ 	if ((b==2) || (b==5) || (b==8))
+ 		cout<<200;
+
+ 	else if (a%2!=0)
+ 		cout<<100;
+ 	
+ 	else if (a==b)
+ 		cout<<50;
+
+	else{
+		cout<<0;
+	}
+	
+return 0;}
