34cd45f0234887c1b781db9736a0346bc9d3f9a0
Allen Ling <87615269+Lcyallen@users.noreply.github.com>
2021-08-01 15:16:54 +0800

    大數相加
    1.http://www.tcgs.tc.edu.tw:1218/ShowProblem?problemid=a031
    2.學習long long int怎麼宣告

---
+++ huge number.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	
+	long long int a, b;
+	
+	char A, B;
+	
+	cin>>a>>b;
+	
+	if (a>b)
+		cout<<'A'<<" "<<a+b;
+		
+	else{
+		cout<<'B'<<" "<<a+b;
+	}
+	
+	return 0;
+}
