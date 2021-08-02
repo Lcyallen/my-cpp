04d08c9891e2b747f9282ebce50375fea97d55db
Allen Ling <87615269+Lcyallen@users.noreply.github.com>
2021-08-02 23:51:03 +0800

    井字矩形
    1.其實這個題目我之前就寫過了，但寫起來竟然還是卡了一下。
    2.又是卡在for迴圈的初始值，我一開始寫在int，但卻應該寫進for的初始條件！
    3.應該反思一下到底寫在哪裡才正確

---
+++ # rectangle.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	int n, m, i, j;
+	
+	cin>>n>>m;
+	
+	for (j=1; j<=m; j++)
+	{
+		for (i=1; i<=n; i++)
+			cout<<"#";
+	
+		cout<<endl;
+	}	
+		
+			
+	return 0;
+}
