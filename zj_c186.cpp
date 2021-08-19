6b3b6e706aa1ad3971d03b19e5487bb99c5bccbf
Allen Ling <87615269+Lcyallen@users.noreply.github.com>
2021-08-19 23:38:58 +0800

    zj_c186
    這應該是最簡單的題目了，但我卻忘記輸入中間有空格，最後再按enter可以直接用while(cin)來處理...真窘

---
+++ zj_c186.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	string a;
+	
+	while(cin>>a)
+	{
+		cout<<a<<endl;
+	}
+	
+	return 0;
+}
+
+//這應該是最簡單的題目了，但我卻忘記輸入中間有空格，最後再按enter可以直接用while(cin)來處理...真窘
