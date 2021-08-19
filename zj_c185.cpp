43c0ec8327792158b7fb6b012c666ccd6aed68f4
Allen Ling <87615269+Lcyallen@users.noreply.github.com>
2021-08-19 23:17:56 +0800

    zj_c185
    複習了getline要怎麼寫(其實忘了)

---
+++ zj_c185.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>	//要用getline必須引入string標頭檔
+using namespace std;
+
+int main (){
+	
+	string a;
+	getline(cin,a);	//注意getline要怎麼寫
+	
+	cout<<"Hey "<<a;
+	
+	
+	return 0;
+}
+//再次複習getline怎麼寫
