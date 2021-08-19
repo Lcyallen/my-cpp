c743b5ec8f81dcda3cc504d8bf82cc0daebc48b9
Allen Ling <87615269+Lcyallen@users.noreply.github.com>
2021-08-19 22:54:40 +0800

    zj_c382
    又一次忘了寫while...

---
+++ zj_c382.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	int a, b;
+	char op;
+	
+	while(cin>>a>>op>>b)
+	{
+		if (op=='+')
+		cout<<a+b<<endl;
+	
+		else if(op=='-')
+		cout<<a-b<<endl;
+	
+		else if (op=='*')
+		cout<<a*b<<endl;
+	
+		else{
+		cout<<a/b<<endl;
+		}
+	}	
+	
+	
+	
+	
+	return 0;
+}
