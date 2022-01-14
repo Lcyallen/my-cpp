bb2ed21f6d61ff580e5d190a796ecf33b8ca6a96
Allen Ling <87615269+Lcyallen@users.noreply.github.com>
2022-01-14 22:11:12 +0800

    Update zj_e971_TOI梗圖著色.cpp

--- zj_e971_TOI梗圖著色.cpp
+++ zj_e971_TOI梗圖著色.cpp
@@ -5,17 +5,17 @@ int main (){
 	int m, n;
 	cin>>m>>n;
 	while (m--){
-		int j=0, a[105]={}, b[105]={-1};//�쥻�H���ۤv���{���g���F�A��ӵo�{�o�Ǫ�l�ȥ������i��while�j��̭�
+		int j=0, a[105]={}, b[105]={-1};//原本以為自己的程式寫錯了，後來發現這些初始值必須移進來while迴圈裡面
 		for (int i=0; i<n; i++){
 			cin>>a[i];
 			if (a[i]==1){
-				b[j]=i;
-				j++; //�ƦC����j��1 
+				b[j]=i;//第2個數列記錄是第幾項為1
+				j++; //數列中有j個1 
 			}
 		}
-		for (int k=0; 2*k<=j; k++){
-			for (int i=0; i<n; i++){
-				if (i>b[2*k] && i<b[2*k+1])
+		for (int k=0; 2*k<j; k++){ //最外圈決定要做幾次下面的程序
+			for (int i=0; i<n; i++){ //每個a[i]都要檢查 
+				if (i>b[2*k] && i<b[2*k+1])//把兩個1之間的數都換成1 
 					a[i]=1;
 			}
 		}
