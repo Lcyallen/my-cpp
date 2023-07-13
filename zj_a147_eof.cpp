02c26a2eb7ea773c4bacb129e88e9498b147f96b
Allen Ling <87615269+Lcyallen@users.noreply.github.com>
2023-07-13 14:16:47 +0800

    Add files via upload
    1.復習cpp的eof怎麼寫

---
+++ zj_a147_eof.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	int a = 0;
+	while(cin>>a, a){
+	for (int i=1; i<a; i++){
+		if (i%7) cout<<i<<" ";
+		}
+	cout<<endl;
+	}
+			
+return 0;
+}
+
+
