154bb0fd2fb12838426445f33450b65f65e6e968
Allen Ling <87615269+Lcyallen@users.noreply.github.com>
2024-07-20 22:00:30 +0800

    zj_a224_3
    1.自己做的小修改
    2.輸入一行字串中間沒有空格時，可以用while(cin >> s)

---
+++ zj_a224_3.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string> //getline也要用
+#include <algorithm> //sort
+#include <vector> //vector
+using namespace std;
+
+int main ()
+{
+	string o;
+	while (cin >> o) //題目的測資要同時輸入很多個，所以要用while。cin後要告知輸入到哪
+	{		
+		string s;	//宣告新的字串s，用來剔除非字母字元
+		int l = o.size();
+
+		for (int i = 0; i < l; i++)
+		{
+			if (o[i] >= 'a' && o[i] <= 'z' || o[i] >= 'A' && o[i] <= 'Z')
+				s += o[i];    //這個寫法很重要！string s不是用[]加入元素。另外，加入元素要+=
+		}
+		
+		for (int i = 0; i < s.size(); i++)
+		{
+			if (s[i] >= 'A' && s[i] <= 'Z')
+				s[i] -=('A'- 'a');  //把大寫字母減('A'-'a')就是他們的間隔，這樣不用記間隔數
+		}
+
+		sort (s.begin(), s.end());  //s是vector，vector是一種容器，要排序要用begin和end
+		//cout << s << endl;
+		vector <int> v = {0};	//讓v[0]=0
+		for (int i = 1; i < s.size(); i++)
+		{
+			if (s[i] != s[i-1])
+				v.push_back(i);
+		}
+		v.push_back(s.size());	//把s的長度加到v的最後
+		//for (int i: v)
+		//cout << i << " ";
+		//cout << endl;
+		int oddnum = 0;
+		for (int i = 1; i < v.size(); i++)
+		{
+			if ((v[i]-v[i-1])%2 == 1)
+				oddnum ++;
+		}
+		if (oddnum > 1) 
+			cout << "no..." << endl;
+		else	
+			cout << "yes !"<< endl;
+	}
+return 0;
+}
